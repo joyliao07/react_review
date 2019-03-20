7da5764126289e7cebcb60db3747916092fe8f5b
Joyce Liao <joyliao07@gmail.com>
2019-03-20 11:24:11 -0700

    adding seperate componenet/folder

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>React app testing</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'app'}, React.createElement('h1', null, 'I\'m using an alternative render method.'))
+    return (
+      <div className="App">
+        <h1>React app testing</h1>
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', {className: 'app'}, React.createElement('h1', null, 'I\'m using an alternative render method.'), React.createElement('p', null, 'second element here.'));
   }
 }
 
---
+++ react-demo/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>Here is a person!</p>;
+}
+
+export default person;
