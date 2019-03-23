c3e4d8af82cc8a5cfbfcc86bebbc1845b50f9bea
Joyce Liao <joyliao07@gmail.com>
2019-03-23 09:27:17 -0700

    add media query

--- react-demo/src/Person/Person.js
+++ react-demo/src/Person/Person.js
@@ -3,16 +3,19 @@ import "./Person.css";
 import Radium from 'radium';
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 550px)': {
+            width: '550px',
+        }
+    }
+
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p>Here is {props.name} who likes to {props.hobby}.</p>
             <p>{props.children}</p>
             <input type="text" placeholder="Type a name" onChange={props.changed}></input>
             <button onClick={props.click}>Remove</button>
         </div>
-        
-        
-        
         )
 };
 
