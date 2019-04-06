3747d2f380d8ff1dba8972af15d9f142a89fd992
Joyce Liao <joyliao07@gmail.com>
2019-04-06 14:09:19 -0700

    Aux, Fragment

--- react-demo/src/components/Persons/Person/Person.js
+++ react-demo/src/components/Persons/Person/Person.js
@@ -1,20 +1,24 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import PersonCss from "./Person.css";
 // import App from '../App';
 import App from '../../../containers/App';
-
+import Aux from '../../../hoc/Aux';
 
 // const person = (props) => {
 class Person extends Component {
     render () {
-        return [
-            // <div className={PersonCss.Person}>
-                <p>Here is {this.props.name} who likes to {this.props.hobby}.</p>,
-                <p>{this.props.children}</p>,
-                <input type="text" placeholder="Type a name" onChange={this.props.changed}></input>, 
+        return (
+            <Fragment>
+            {/* // <Aux> */}
+            {/* // <div className={PersonCss.Person}> */}
+                <p>Here is {this.props.name} who likes to {this.props.hobby}.</p>
+                <p>{this.props.children}</p>
+                <input type="text" placeholder="Type a name" onChange={this.props.changed}></input> 
                 <button onClick={this.props.click} className={PersonCss.Button}>Remove</button>
-            // </div>
-        ];
+            {/* // </div> */}
+            {/* </Aux> */}
+            </Fragment>
+        );
     }
 }
 
