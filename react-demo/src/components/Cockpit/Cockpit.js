ca6f35a48aec3d9e1e7231e1fb7bd09e91315625
Joyce Liao <joyliao07@gmail.com>
2019-04-08 10:44:09 -0700

    add Ref with React Hooks useRef()

--- react-demo/src/components/Cockpit/Cockpit.js
+++ react-demo/src/components/Cockpit/Cockpit.js
@@ -1,9 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import CockpitCss from './Cockpit.css';
 
 
 const cockpit = (props) => {
     
+    const toggleBtnRef = useRef(null);
+    
+    useEffect(() => {
+      // We need to put this command line inside useEffect because useEffect is run after each complete render cycle; so this <toggleBtnRef.current.click();> is run after the entire JSX code has been rendered.
+      toggleBtnRef.current.click();
+    });
+
+
     // useEffect(() => {
     //   console.log('useEffect');
     //   // HTTP request..
@@ -45,7 +53,8 @@ const cockpit = (props) => {
             <h1 className={assignedClasses.join(' ')}> {props.title} </h1>
             <button 
             onClick={props.clicked}
-            className={CockpitCss.Button}>Show/Hide</button>
+            className={CockpitCss.Button}
+            ref={toggleBtnRef}>Show/Hide</button>
         </div>
     );
 };
