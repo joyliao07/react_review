82679040be6a2874a04fb96281a6ad30a2698a2b
Joyce Liao <joyliao07@gmail.com>
2019-04-07 17:24:52 -0700

    add reference to elements

--- react-demo/src/components/Persons/Person/Person.js
+++ react-demo/src/components/Persons/Person/Person.js
@@ -7,6 +7,17 @@ import PropTypes from 'prop-types';
 
 // const person = (props) => {
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render () {
         return (
             <Aux>
@@ -14,7 +25,13 @@ class Person extends Component {
             {/* // <Fragment className={PersonCss.Person}> */}
                 <p>Here is {this.props.name} who likes to {this.props.hobby}.</p>
                 <p>{this.props.children}</p>
-                <input type="text" placeholder="Type a name" onChange={this.props.changed}></input> 
+                <input 
+                    type="text"
+                    placeholder="Type a name" 
+                    onChange={this.props.changed}
+                    // ref = {(inputEl) => {this.inputElement = inputEl}}
+                    ref = {this.inputElementRef}
+                    /> 
                 <button onClick={this.props.click} className={PersonCss.Button}>Remove</button>
             {/* </Fragment> */}
             {/* </div> */}
