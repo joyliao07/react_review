17890d92888d1ca5d301b7486341158137db00f0
Joyce Liao <joyliao07@gmail.com>
2019-05-13 18:33:40 -0700

    add backdrop when Modal is shown

--- react-burger/src/components/UI/Modal/Modal.js
+++ react-burger/src/components/UI/Modal/Modal.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import classes from './Modal.css'
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => {
     return (
-        <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                ocapcity: props.show ? '1' : '0',
-            }}
-        > { props.children }
-        </div>
+        <Aux>
+            <Backdrop show={props.show}/>
+            <div className={classes.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    ocapcity: props.show ? '1' : '0',
+                }}
+                > { props.children }
+            </div>
+        </Aux>
         
     );
 
