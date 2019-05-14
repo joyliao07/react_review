b6f3b16895a30d1ccc1ddbfbd3cc68e59dea8950
Joyce Liao <joyliao07@gmail.com>
2019-05-14 11:00:41 -0700

    add separate button component

--- react-burger/src/components/UI/Button/Button.js
+++ react-burger/src/components/UI/Button/Button.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import classes from './Button.css';
 
 const button = (props) => (
-    <button onClick={props.clicked}> {props.children} </button>
+    <button 
+        onClick={props.clicked}
+        className={[classes.Button, classes[props.btnType]].join(' ')}
+        > {props.children} </button>
 );
 
 export default button;
