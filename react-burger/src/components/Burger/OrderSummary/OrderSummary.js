6f1e9edf8a2159eec39331d056f01e449b7857f6
Joyce Liao <joyliao07@gmail.com>
2019-05-17 15:02:34 -0700

    order-summary componentShouldUpdate

--- react-burger/src/components/Burger/OrderSummary/OrderSummary.js
+++ react-burger/src/components/Burger/OrderSummary/OrderSummary.js
@@ -35,30 +35,4 @@ class OrderSummary extends Component {
     }
 } 
 
-// => {
-//     const ingredientSummary = Object.keys(props.ingredients)
-//         .map(key => {
-//             return (
-//                 <li key={key}>
-//                     <span style={{textTransform: 'capitalize'}}>{key}</span>: {props.ingredients[key]}
-//                 </li>
-//             );
-//         });
-
-//     return (
-//         <Aux>
-//             <h3>Your Order:</h3>
-//                 <ul>
-//                     {ingredientSummary}
-//                 </ul>
-
-//             <p><strong>Total Price: $ {props.total.toFixed(2)} </strong></p>
-//             <p>Continue to check out?</p>
-//             <Button btnType="Danger" clicked={props.cancel}>Cancel</Button>
-//             <Button btnType="Success" clicked={props.continue}>Continue</Button>
-
-//         </Aux>
-//     );
-// }
-
 export default OrderSummary;
