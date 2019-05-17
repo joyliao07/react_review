0ca5f63cf74fbd172be5b36095864a492ce75944
Joyce Liao <joyliao07@gmail.com>
2019-05-16 18:52:47 -0700

    hide backdrop upon click

--- react-burger/src/App.js
+++ react-burger/src/App.js
@@ -3,10 +3,20 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+    state = {
+      showSideDrawer: true,
+    }
+
+    sideDrawerClosedHandler = () => {
+      this.setState({showSideDrawer: false});
+    }
+
   render() {
     return (
       <div>
-        <Layout>
+        <Layout 
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}>
           <BurgerBuilder/>
         </Layout>
       </div>
--- react-burger/src/components/Layout/Layout.js
+++ react-burger/src/components/Layout/Layout.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import ToolBar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <ToolBar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+const layout = (props) => {
+// class Layout extends Component {
+    // state = {
+    //     showSideDrawer: true,
+    // }
+
+    // sideDrawerClosedHandler = () => {
+    //     this.setState({showSideDrawer: false});
+    // }
+
+    // reder () {
+        return(
+            // <div>
+            //     ABC
+            // </div>
+            <Aux>
+                <ToolBar/>
+                <SideDrawer 
+                    open={props.open} 
+                    closed={props.closed}
+                    />
+                <main className={classes.Content}>
+                    {props.children}
+                </main>
+            </Aux>
+        );
+    // }
+}
 
 export default layout;
