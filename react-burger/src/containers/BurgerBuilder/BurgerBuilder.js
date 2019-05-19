3e5e6771f8c2191828602d02f043b7c51a4b5335
Joyce Liao <joyliao07@gmail.com>
2019-05-18 19:38:10 -0700

    spinner css from single element

--- react-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -55,6 +55,7 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
+        this.setState({loading: true});
         const order = {
             ingredients: this.state.ingredients,
             // in a real production, the price should be calcued on the server to avoid manipulation of prices by the user
@@ -70,8 +71,19 @@ class BurgerBuilder extends Component{
             }
         }
         axios.post('/orders.json', order)
-            .then( response => console.log(response))
-            .catch( error => console.log(error));
+            .then( response => {
+                this.setState({
+                    loading: false,
+                    purchasing: false,
+                });
+            })
+            .catch( error => {
+                this.setState({
+                    loading: false,
+                    purchasing: false,
+                });
+                console.log(error);
+            });
     }
 
     addIngredientHandler = (type) => {
